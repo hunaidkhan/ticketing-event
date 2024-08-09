317b6a3a7e915cfab85665e0b60ed791f7f7e943
hunaidkhan <hunaid20042004@gmail.com>
2024-08-09 16:54:32 -0600

    modified file structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
 
-const poppins = Poppins({
-   subsets: ["latin"],
-   weight: ['400', '500', '600', '700'],
-   variable: '--font-poppins'
-   });
+
+import Header from "@/components/shared/header";
+import Footer from "@/components/shared/footer";
+
 
 export const metadata: Metadata = {
   title: "Muslims Student Association (MSA)",
@@ -22,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <div className="flex h-screen flex-col">
+    <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">MSA Events</h1>
       <Button variant='outline' className="px-10">YEEE</Button>
     </main>
   );
