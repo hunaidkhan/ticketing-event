ca574e66dd0f0c77988b5cc66a3fbb11378ffe93
hunaidkhan <hunaid20042004@gmail.com>
2024-08-16 17:30:59 -0600

    fixed auth bugs

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type EventFormProps = {
+    userId: string,
+    type: 'Create' | 'Update'
+}
+
+const EventForm = ({userId, type}: EventFormProps) => {
+  return (
+    <div>EventForm {type}</div>
+  )
+}
+
+export default EventForm
